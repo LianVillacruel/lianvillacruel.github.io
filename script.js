ec64a01e4839f88db2543bfda97137eeab5a4762
LianVillacruel <lianbaladiavillacruel@gmail.com>
2026-02-16 21:03:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-alert("This Website is not done yet. Still working on it!");
+alert(
+  "This website is not an official portfolio, but a draft for my future portfolio. I am still working on it, I will make an final one in the future. Thank you for your understanding!",
+);
